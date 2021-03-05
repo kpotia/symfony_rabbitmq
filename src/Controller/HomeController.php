49092f3eadc2d84325614a82b5c916ccbd02e05b
Mr Kpoti <patrickamtepe@gmail.com>
2021-03-05 21:46:21 +0000

    add incident and mailer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,17 +4,51 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Incident;
+use App\Form\IncidentType;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em, MailerInterface $mailer): Response
     {
+        $task  = new Incident();
+        $task->setUser($this->getUser())
+        	 ->setCreatedAt(new \DateTime('now'));
+        $form = $this->createForm(IncidentType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+        	$task = $form->getData();
+
+        	$em->persist($task);
+        	$em->flush();
+
+        	// sleep(10);
+
+        	$email = (new Email())
+        	->from($task->getUser()->getEmail())
+        	->to('you@example.com')
+        	->subject('New Incident #'. $task->getId(). ' - '. $task->getUser()->getEmail())
+        	->html('<p>'. $task->getDescription() . '</p>');
+
+        	sleep(10);
+        	$mailer->send($email);
+
+        	return $this->redirectToRoute('home');
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        	'form' => $form->createView(),
+
         ]);
     }
 }
--- src/Form/IncidentType.php
+++ src/Form/IncidentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Incident;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,8 @@ class IncidentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('createdAt')
             ->add('description')
-            ->add('user')
+            ->add('submit', SubmitType::class)
         ;
     }
 
